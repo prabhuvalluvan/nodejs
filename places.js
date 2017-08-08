cf9bfc9e56f88b662f94bb79b38aca555da6c168
Selva Valluvan <selva.valluvan@quickmobile.com>
2017-08-08 10:57:57 -0700

    Fixing error

--- places.js
+++ places.js
@@ -1,35 +1,34 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 
 app.get('/', function (req, res) {
-   var key ="AIzaSyDwrHGHKWUjDvdcI5-mLBdqDP9gk1L_XeI";
-  var location = encodeURIComponent(req.query.location);
-  var radius = 16000;
-  var sensor = false;
-  var types = "restaurant";
-  var keyword = "fast";
+    var key = 'AIzaSyDwrHGHKWUjDvdcI5-mLBdqDP9gk1L_XeI';
+    var location = '-33.8670522,151.1957362';
+    var radius = 500;
+    var type = 'point_of_interest';
 
-  var https = require('https');
-  var url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?" + "key=" + key + "&location=" + location + "&radius=" + radius + "&sensor=" + sensor + "&types=" + types + "&keyword=" + keyword;
+    var https = require('https');
+    var url = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?' + 'key=' + key + '&location=' + location + '&radius=' + radius + '&type=' + type;
     console.log(url);
-  https.get(url, function(response) {
-    var body ='';
-    response.on('data', function(chunk) {
-      body += chunk;
-    });
+    https.get(url, function (response) {
+        var body = '';
+        response.on('data', function (chunk) {
+            body += chunk;
+        });
 
-    response.on('end', function() {
-      var places = JSON.parse(body);
-      var locations = places.results;
-      var randLoc = locations[Math.floor(Math.random() * locations.length)];
+        response.on('end', function () {
+            // var places = JSON.parse(body);
+            // var locations = places.results;
+            // var randLoc = locations[Math.floor(Math.random() * locations.length)];
 
-      res.json(randLoc);
+            res.json(JSON.parse(body));
+        });
+    }).on('error', function (e) {
+        console.log('Got error: ' + e.message);
+        res.json({error: e.message});
     });
-  }).on('error', function(e) {
-    console.log("Got error: " + e.message);
-  });
-})
+});
 
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
-})
+    console.log('Example app listening on port 3000!');
+});
